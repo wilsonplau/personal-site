e45842999d6fe7d6b1ad18c09e64b330280c9ac3
wilsonplau <hello@wilsonplau.com>
2021-07-27 13:11:01 +0900

    feat: add head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,20 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 import "../styles/Reset.scss";
 import "../styles/App.scss";
 import "aos/dist/aos.css";
 
 const CustomApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<meta charSet="utf-8" />
+				<title>Wilson Lau</title>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 };
 
 export default CustomApp;
